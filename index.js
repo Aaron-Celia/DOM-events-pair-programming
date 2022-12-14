a24851dc5bc33c320ab3f78a36a665a38262666a
Aaron Celia <113921205+Aaron-Celia@users.noreply.github.com>
2022-12-14 17:18:00 -0500

    crane game attempt added

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-//cookie click count
 const cookieButton = document.querySelector("#cookie-button");
 const counterDiv = document.querySelector("#counter");
 let  counterNum = 0;
@@ -34,5 +33,42 @@ document.querySelector("#input-box").style.marginBottom = "10px";
 colorBttn.addEventListener("click", function(){
     let input = document.querySelector("#input-box").value;
     document.body.style.backgroundColor = input;
+});
+
+
+
+//crane game
+const box = document.querySelector("#container");
+//box styling
+box.style.backgroundColor = "#333";
+box.style.height = "400px";
+box.style.width = "250px";
+
+let crane = document.querySelector("#crane-image");
+//image styles
+crane.style.width = "50%"
+crane.style.height = "50%"
+// crane.style.position = "relative";
+
+let topButton = document.querySelector("#top");
+let bottomButton = document.querySelector("#bottom");
+let leftButton = document.querySelector("#left");
+let rightButton = document.querySelector("#right");
+
+// click events
+topButton.addEventListener("click", function(){
+    crane.setAttribute("align", "top")
+});
+
+bottomButton.addEventListener("click", function(){
+    crane.setAttribute("align", "bottom");
+});
+
+leftButton.addEventListener("click", function(){
+    crane.className = "left";
+});
+
+rightButton.addEventListener("click", function(){
+    crane.className = "right"
 });
 
